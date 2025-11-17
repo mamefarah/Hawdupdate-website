47e4d21899d5091dd7922cad8153ff380a6fed4a
Muhammed Farah <93242587+mamefarah@users.noreply.github.com>
2025-11-17 21:01:12 +0300

    Update sanity.config.ts

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,2 +1,25 @@
-placeholder
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { visionTool } from "@sanity/vision";
 
+import mission from "./schemas/mission";
+import vision from "./schemas/vision";
+import about from "./schemas/about";
+import services from "./schemas/services";
+import team from "./schemas/team";
+import projects from "./schemas/projects";
+import homepage from "./schemas/homepage";
+
+export default defineConfig({
+  name: "hawdclimateguardian",
+  title: "Hawd Climate Guardian CMS",
+
+  projectId: "YOUR_PROJECT_ID",
+  dataset: "production",
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: [mission, vision, about, services, team, projects, homepage],
+  },
+});
