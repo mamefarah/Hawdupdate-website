4c9be0730bf4a2ff53448a45efaeee2cc0c0c6ae
Muhammed Farah <93242587+mamefarah@users.noreply.github.com>
2025-11-17 21:03:31 +0300

    Update services.ts

--- sanity/schemas/services.ts
+++ sanity/schemas/services.ts
@@ -1,1 +1,11 @@
+export default {
+  name: "services",
+  title: "Services",
+  type: "document",
+  fields: [
+    { name: "name", type: "string", title: "Service Name" },
+    { name: "description", type: "text", title: "Description" },
+    { name: "icon", type: "image", title: "Icon" }
+  ]
+};
 
