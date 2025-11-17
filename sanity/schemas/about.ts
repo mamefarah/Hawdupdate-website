195aaad47314d954e54cc732f88152dd652ba7ea
Muhammed Farah <93242587+mamefarah@users.noreply.github.com>
2025-11-17 21:03:10 +0300

    Update about.ts

--- sanity/schemas/about.ts
+++ sanity/schemas/about.ts
@@ -1,1 +1,11 @@
+export default {
+  name: "about",
+  title: "About",
+  type: "document",
+  fields: [
+    { name: "title", type: "string", title: "Title" },
+    { name: "body", type: "array", title: "Body", of: [{ type: "block" }] },
+    { name: "image", type: "image", title: "Image" }
+  ]
+};
 
