993d571d489bf50a5d1980bc6abd27cd98d25edf
Muhammed Farah <93242587+mamefarah@users.noreply.github.com>
2025-11-17 21:02:57 +0300

    Update vision.ts

--- sanity/schemas/vision.ts
+++ sanity/schemas/vision.ts
@@ -1,1 +1,10 @@
+export default {
+  name: "vision",
+  title: "Vision",
+  type: "document",
+  fields: [
+    { name: "title", type: "string", title: "Title" },
+    { name: "description", type: "text", title: "Description" }
+  ]
+};
 
