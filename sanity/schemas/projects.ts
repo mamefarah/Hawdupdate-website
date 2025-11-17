5b277d60ba223e10c2f3df16e40c429940342917
Muhammed Farah <93242587+mamefarah@users.noreply.github.com>
2025-11-17 21:05:09 +0300

    Update projects.ts

--- sanity/schemas/projects.ts
+++ sanity/schemas/projects.ts
@@ -1,1 +1,12 @@
+export default {
+  name: "projects",
+  title: "Projects",
+  type: "document",
+  fields: [
+    { name: "title", type: "string", title: "Title" },
+    { name: "description", type: "text", title: "Description" },
+    { name: "image", type: "image", title: "Image" },
+    { name: "location", type: "string", title: "Location" }
+  ]
+};
 
