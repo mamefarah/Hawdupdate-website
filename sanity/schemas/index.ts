fefd3c1ff08b175c0065c6ec72af7268607e8a04
Muhammed Farah <93242587+mamefarah@users.noreply.github.com>
2025-11-17 21:05:50 +0300

    Update index.ts

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,5 +1,17 @@
-/sanity
-  sanity.config.ts
-  /schemas
-      index.ts
+import mission from "./mission";
+import vision from "./vision";
+import about from "./about";
+import services from "./services";
+import team from "./team";
+import projects from "./projects";
+import homepage from "./homepage";
 
+export const schemaTypes = [
+  mission,
+  vision,
+  about,
+  services,
+  team,
+  projects,
+  homepage
+];
