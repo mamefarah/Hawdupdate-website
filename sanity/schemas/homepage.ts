8fc839a51746ceb1762c7463d0f0d1ad3d67f70b
Muhammed Farah <93242587+mamefarah@users.noreply.github.com>
2025-11-17 21:05:25 +0300

    Update homepage.ts

--- sanity/schemas/homepage.ts
+++ sanity/schemas/homepage.ts
@@ -1,1 +1,11 @@
+export default {
+  name: "homepage",
+  title: "Homepage",
+  type: "document",
+  fields: [
+    { name: "headline", type: "string", title: "Headline" },
+    { name: "subheadline", type: "string", title: "Subheadline" },
+    { name: "heroImage", type: "image", title: "Hero Image" }
+  ]
+};
 
