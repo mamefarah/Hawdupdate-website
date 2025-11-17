41cf5447af0de1a79bf7da706eddaa94a8d166a7
Muhammed Farah <93242587+mamefarah@users.noreply.github.com>
2025-11-17 21:02:44 +0300

    Update mission.ts

--- sanity/schemas/mission.ts
+++ sanity/schemas/mission.ts
@@ -1,2 +1,9 @@
-placeholder
-
+export default {
+  name: "mission",
+  title: "Mission",
+  type: "document",
+  fields: [
+    { name: "title", type: "string", title: "Title" },
+    { name: "description", type: "text", title: "Description" }
+  ]
+};
